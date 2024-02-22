5975985dd35ef8ceea10b84da91f85ea93cef20b
Alina <alina.shamilevna@gmail.com>
2024-02-22 13:21:29 +0300

    docs: Add docstrings for functions in square.py

--- square.py
+++ square.py
@@ -1,7 +1,23 @@
 
 def area(a):
+""" Returns square area with given side.
+    
+    parameter r: square side
+    type r: float
+
+    return: square area
+    return type: float
+    """
     return a * a
 
 
 def perimeter(a):
+""" Returns square perimeter with given side.
+    
+    parameter r: square side
+    type r: float
+
+    return: square area
+    return type: float
+    """
     return 4 * a
