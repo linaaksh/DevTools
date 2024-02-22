31695fa80f0370c19e8bef99bf060bfada0d139e
Alina <alina.shamilevna@gmail.com>
2024-02-22 13:21:15 +0300

    docs: Add docstrings for functions in rectangle.py

--- rectangle.py
+++ rectangle.py
@@ -1,5 +1,25 @@
 def area(a, b): 
+    """ Returns rectangle area with given sides.
+    
+    parameter a: first side
+    parameter b: second side
+    type a: float
+    type b: float
+
+    return: rectangle area
+    return type: float
+    """
     return a * b 
 
-def perimeter(a, b): 
+def perimeter(a, b):
+    """ Returns rectangle perimeter with given sides.
+    
+    parameter a: first side
+    parameter b: second side
+    type a: float
+    type b: float
+
+    return: rectangle area
+    return type: float
+    """
     return (a + b) * 2
