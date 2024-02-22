5bd1427e6a5f74916b00ef1228b5862564814a24
Alina <alina.shamilevna@gmail.com>
2024-02-22 13:20:59 +0300

    docs: Add docstrings for functions in triangle.py

--- triangle.py
+++ triangle.py
@@ -1,5 +1,27 @@
-def area(a, h): 
+def area(a, h):
+    """ Returns triangle area with given base and height.
+    
+    parameter a: base
+    parameter h: height
+    type a: float
+    type h: float
+
+    return: triangle area
+    return type: float
+    """
     return a * h / 2 
 
-def perimeter(a, b, c): 
+def perimeter(a, b, c):
+    """ Returns triangle perimeter with given sides.
+    
+    parameter a: first side
+    parameter b: second side
+    parameter c: third side
+    type a: float
+    type b: float
+    type c: float
+
+    return: triangle perimeter
+    return type: float
+    """
     return a + b + c 
